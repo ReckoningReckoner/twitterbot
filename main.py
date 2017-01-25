d54d4c2aa3ece48b220631a266a51e5a9694847b
Viraj Bangari <Viraj3f@gmail.com>
2017-01-25 18:38:49 -0500

    Modify readme

--- main.py
+++ main.py
@@ -4,20 +4,21 @@ import sys
 
 tweets = open("./tweets.txt").readlines()
 last_index = open("./lastindex.txt", "r+")
-time_delay = 1
+time_delay = 0.5
 
 i = int(last_index.readline())
 while True:
     try:
         tweet = tweets[i].rstrip()
         send_tweet(tweet)
-        time.sleep(1)
+        time.sleep(time_delay)
         if i < len(tweets) - 1:
             i += 1
         else:
             i = 0
-    except:
+    except Exception as e:
         last_index.truncate()
         last_index.seek(0)
         last_index.write(str(i + 1))
+        print(e)
         sys.exit()
